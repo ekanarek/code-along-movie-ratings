42851006316d09f43323d4ea8f83715bffedc237
Emily Kanarek <emilyakanarek@gmail.com>
2024-07-17 17:12:49 -0400

    Seed database

---
+++ scripts/seed.js
@@ -0,0 +1,23 @@
+import { Movie, Rating, User, db } from '../src/model.js';
+import movieData from './data/movies.json' assert { type: 'json' };
+
+console.log('Syncing database...');
+await db.sync({ force: true });
+
+console.log('Seeding database...');
+
+const moviesInDB = movieData.map(async (movie) => {
+    const releaseDate = new Date(Date.parse(movie.releaseDate));
+    const { title, overview, posterPath } = movie;
+
+    const newMovie = await Movie.create({
+        title,
+        overview,
+        posterPath,
+        releaseDate,
+    });
+
+    return newMovie;
+});
+
+console.log(moviesInDB);
